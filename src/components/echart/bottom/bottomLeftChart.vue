f731ba998f97e20cb4840b0b2cdc1b1aea7d90b2
liangjiachen <1074847834@qq.com>
2024-03-29 16:35:35 +0800

    demo

--- src/components/echart/bottom/bottomLeftChart.vue
+++ src/components/echart/bottom/bottomLeftChart.vue
@@ -1,6 +1,7 @@
 <template>
-  <div>
-    <div id="bottomLeftChart" style="width:900px;height:500px;"></div>
+  <div style="display: flex;">
+    <div id="bottomLeftChart1" style="width:900px;height:500px;"></div>
+    <div id="bottomLeftChart2" style="width:900px;height:500px;"></div>
   </div>
 </template>
 
@@ -16,249 +17,137 @@ export default {
   methods: {
     drawPie() {
       // 基于准备好的dom，初始化echarts实例
-      let myChartPieLeft = echarts.init(
-        document.getElementById("bottomLeftChart")
+      let barChartLeft = echarts.init(
+        document.getElementById("bottomLeftChart1")
+      );
+      let barChartRight = echarts.init(
+        document.getElementById("bottomLeftChart2")
       );
       //  ----------------------------------------------------------------
-      let category = [
-        "市区",
-        "万州",
-        "江北",
-        "南岸",
-        "北碚",
-        "綦南",
-        "长寿",
-        "永川",
-        "璧山",
-        "江津",
-        "城口",
-        "大足",
-        "垫江",
-        "丰都",
-        "奉节",
-        "合川",
-        "江津区",
-        "开州",
-        "南川",
-        "彭水",
-        "黔江",
-        "石柱",
-        "铜梁",
-        "潼南",
-        "巫山",
-        "巫溪",
-        "武隆",
-        "秀山",
-        "酉阳",
-        "云阳",
-        "忠县",
-        "川东",
-        "检修"
-      ];
-      let lineData = [
-        18092,
-        20728,
-        24045,
-        28348,
-        32808,
-        36097,
-        39867,
-        44715,
-        48444,
-        50415,
-        56061,
-        62677,
-        59521,
-        67560,
-        18092,
-        20728,
-        24045,
-        28348,
-        32808,
-        36097,
-        39867,
-        44715,
-        48444,
-        50415,
-        36097,
-        39867,
-        44715,
-        48444,
-        50415,
-        50061,
-        32677,
-        49521,
-        32808
-      ];
-      let barData = [
-        4600,
-        5000,
-        5500,
-        6500,
-        7500,
-        8500,
-        9900,
-        12500,
-        14000,
-        21500,
-        23200,
-        24450,
-        25250,
-        33300,
-        4600,
-        5000,
-        5500,
-        6500,
-        7500,
-        8500,
-        9900,
-        22500,
-        14000,
-        21500,
-        8500,
-        9900,
-        12500,
-        14000,
-        21500,
-        23200,
-        24450,
-        25250,
-        7500
-      ];
-      let rateData = [];
-      for (let i = 0; i < 33; i++) {
-        let rate = barData[i] / lineData[i];
-        rateData[i] = rate.toFixed(2);
-      }
-
-      let option = {
-        title: {
-          text: "",
-          x: "center",
-          y: 0,
-          textStyle: {
-            color: "#B4B4B4",
-            fontSize: 16,
-            fontWeight: "normal"
-          }
-        },
-        tooltip: {
-          trigger: "axis",
-          backgroundColor: "rgba(255,255,255,0.1)",
-          axisPointer: {
-            type: "shadow",
-            label: {
-              show: true,
-              backgroundColor: "#7B7DDC"
-            }
-          }
-        },
-        legend: {
-          data: ["已贯通", "计划贯通", "贯通率"],
-          textStyle: {
-            color: "#B4B4B4"
-          },
-          top: "0%"
-        },
-        grid: {
-          x: "8%",
-          width: "88%",
-          y: "4%"
-        },
-        xAxis: {
-          data: category,
-          axisLine: {
-            lineStyle: {
-              color: "#B4B4B4"
-            }
-          },
-          axisTick: {
-            show: false
-          }
-        },
-        yAxis: [
-          {
-            splitLine: { show: false },
-            axisLine: {
-              lineStyle: {
-                color: "#B4B4B4"
-              }
-            },
-
-            axisLabel: {
-              formatter: "{value} "
-            }
-          },
-          {
-            splitLine: { show: false },
-            axisLine: {
-              lineStyle: {
-                color: "#B4B4B4"
-              }
-            },
-            axisLabel: {
-              formatter: "{value} "
-            }
-          }
-        ],
-        series: [
-          {
-            name: "贯通率",
-            type: "line",
-            smooth: true,
-            showAllSymbol: true,
-            symbol: "emptyCircle",
-            symbolSize: 8,
-            yAxisIndex: 1,
-            itemStyle: {
-              normal: {
-                color: "#F02FC2"
-              }
-            },
-            data: rateData
-          },
-
-          {
-            name: "已贯通",
-            type: "bar",
-            barWidth: 10,
-            itemStyle: {
-              normal: {
-                barBorderRadius: 5,
-                color: new echarts.graphic.LinearGradient(0, 0, 0, 1, [
-                  { offset: 0, color: "#956FD4" },
-                  { offset: 1, color: "#3EACE5" }
-                ])
-              }
-            },
-            data: barData
-          },
-
-          {
-            name: "计划贯通",
-            type: "bar",
-            barGap: "-100%",
-            barWidth: 10,
-            itemStyle: {
-              normal: {
-                barBorderRadius: 5,
-                color: new echarts.graphic.LinearGradient(0, 0, 0, 1, [
-                  { offset: 0, color: "rgba(156,107,211,0.8)" },
-                  { offset: 0.2, color: "rgba(156,107,211,0.5)" },
-                  { offset: 1, color: "rgba(156,107,211,0.2)" }
-                ])
-              }
-            },
-            z: -12,
-
-            data: lineData
-          }
-        ]
-      };
-      myChartPieLeft.setOption(option);
+      const rawData = [
+  [100, 302, 301, 334, 390, 330, 320],
+  [320, 132, 101, 134, 90, 230, 210],
+  [220, 182, 191, 234, 290, 330, 310],
+  [150, 212, 201, 154, 190, 330, 410],
+  [820, 832, 901, 934, 1290, 1330, 1320]
+];
+const totalData = [];
+for (let i = 0; i < rawData[0].length; ++i) {
+  let sum = 0;
+  for (let j = 0; j < rawData.length; ++j) {
+    sum += rawData[j][i];
+  }
+  totalData.push(sum);
+}
+const grid = {
+  left: 100,
+  right: 100,
+  top: 50,
+  bottom: 50
+};
+const series = [
+  'HTTP',
+  'TCP',
+  'DNS',
+  'FTP',
+  'UDP'
+].map((name, sid) => {
+  return {
+    name,
+    type: 'bar',
+    stack: 'total',
+    barWidth: '60%',
+    label: {
+      show: false,
+    },
+    data: rawData[sid].map((d, did) =>
+      totalData[did] <= 0 ? 0 : d 
+    )
+  };
+});
+let option1 = {
+  title: {
+    text: '目的IP地址基数',
+    textStyle: {
+      color: "rgba(255,255,255,.8)",
+      fontSize: 12
+    }
+  },
+  tooltip: {
+    trigger: 'axis',
+    axisPointer: {
+      type: 'shadow'
+    }
+  },
+  legend: {
+    show: true,
+    left:'100px',
+    selectedMode: false,
+    textStyle: {
+      color: "rgba(255,255,255,.8)",
+      fontSize: 12
+    },
+  },
+  grid,
+  yAxis: {
+    type: 'value',
+    axisLabel: {
+      color: "rgba(255,255,255,.8)",
+      fontSize: 12
+    }
+  },
+  xAxis: {
+    axisLabel: {
+      show:true,
+      color: "rgba(255,255,255,.8)",
+      width: 150,
+      margin:0,
+      overflow:'truncate',
+      ellipsis: '...',
+    },
+    type: 'category',
+    data: ['1.1.1.1','1.1.1.1','1.1.1.1','1.1.1.1','1.1.1.1','1.1.1.1','1.1.1.1']
+  },
+  series
+};
+let option2= {
+  title: {
+    text: '源IP地址基数',
+    textStyle: {
+      color: "rgba(255,255,255,.8)",
+      fontSize: 12
+    }
+  },
+  tooltip: {
+    trigger: 'axis',
+    axisPointer: {
+      type: 'shadow'
+    }
+  },
+  grid,
+  yAxis: {
+    type: 'value',
+    axisLabel: {
+      color: "rgba(255,255,255,.8)",
+      fontSize: 12
+    }
+  },
+  xAxis: {
+    axisLabel: {
+      show:true,
+      color: "rgba(255,255,255,.8)",
+      fontSize: 12
+    },
+    type: 'category',
+    data: ['10.12.3.1', '10.12.3.1', '10.12.3.1', '10.12.3.1', '10.12.3.1', '10.12.3.1', '10.12.3.1']
+  },
+  series
+};
+      barChartLeft.setOption(option1);
+      barChartRight.setOption(option2);
       // -----------------------------------------------------------------
       // 响应式变化
-      window.addEventListener("resize", () => myChartPieLeft.resize(), false);
+      window.addEventListener("resize", () => barChartLeft.resize(), false);
     }
   },
   destroyed() {
